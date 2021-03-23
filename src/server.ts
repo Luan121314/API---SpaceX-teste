0c802c6f9610cd5bafbe5548aea30688be30374e
luan.staner <luan.staner@gmail.com>
2021-03-23 18:01:19 -0300

    config  environnments

--- src/server.ts
+++ src/server.ts
@@ -2,25 +2,21 @@ import express from 'express';
 import 'express-async-errors';
 import routes  from './routes';
 import cors from 'cors';
-import dotenv  from 'dotenv';
 import errorHandler from './errors/handler'
 import database from './services/database/connection';
+import dotenv from 'dotenv';
 
-if(process.env.NODE_ENV != "PRODUCTION"){
-    dotenv.config()
-    console.log('Enviroment variable loading');
-}
+if(process.env.NODE_ENV !== "PRODUCTION") dotenv.config();
 
 const app = express();
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 app.use(routes);
-database();
 app.use(errorHandler);
-
+database();
 
 const port = process.env.PORT || 3333;
-
 app.listen(port, ()=>{
-    console.log(`Running server in port ${port}`);
+    console.log(`Server running in port ${port} `);
 });
+
